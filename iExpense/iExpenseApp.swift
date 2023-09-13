679d6fa7e062ae8ddb309eab246a44e356a948fa
shomil03 <shomil02020@gmail.com>
2023-09-13 22:19:52 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Shomil Singh on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
